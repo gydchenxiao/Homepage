3dbaf908184b8cee52bf5545a99005f634626e8a
小咕咚 <122521272+gudong1012@users.noreply.github.com>
2024-08-12 19:38:29 +0800

    Update set.js

--- js/set.js
+++ js/set.js
@@ -1,111 +1,71 @@
-// 背景图片 Cookies 
-function setBgImg(bg_img) {
-    if (bg_img) {
-        Cookies.set('bg_img', bg_img, {
-            expires: 36500
-        });
-        return true;
-    }
-    return false;
+function setBgImg(a) {
+  return !!a && (Cookies.set("bg_img", a, { expires: 36500 }), !0);
 }
-
-// 获取背景图片 Cookies
 function getBgImg() {
-    var bg_img_local = Cookies.get('bg_img');
-    if (bg_img_local && bg_img_local !== "{}") {
-        return JSON.parse(bg_img_local);
-    } else {
-        setBgImg(bg_img_preinstall);
-        return bg_img_preinstall;
-    }
+  var a = Cookies.get("bg_img");
+  return a && "{}" !== a
+    ? JSON.parse(a)
+    : (setBgImg(bg_img_preinstall), bg_img_preinstall);
 }
-
-var bg_img_preinstall = {
-    "type": "1", // 1:默认背景 2:每日一图 3:随机风景 4:随机动漫
-    "path": "", //自定义图片
-};
-
-
-// 更改背景图片
 function setBgImgInit() {
-    var bg_img = getBgImg();
-    $("input[name='wallpaper-type'][value=" + bg_img["type"] + "]").click();
-
-    switch (bg_img["type"]) {
-        case "1":
-            var pictures = new Array();
-            pictures[0] = './img/background1.webp';
-            pictures[1] = './img/background2.webp';
-            pictures[2] = './img/background3.webp';
-            pictures[3] = './img/background4.webp';
-            pictures[4] = './img/background5.webp';
-            pictures[5] = './img/background6.webp';
-            pictures[6] = './img/background7.webp';
-            pictures[7] = './img/background8.webp';
-            var rd = Math.floor(Math.random() * 8);
-            $('#bg').attr('src', pictures[rd]) //随机默认壁纸
-            break;
-        case "2":
-            $('#bg').attr('src', 'https://bing.img.run/1920x1080.php'); //必应每日
-            break;
-        case "3":
-            $('#bg').attr('src', 'https://api.xsot.cn/bing?jump=true'); //随机风景
-            break;
-        case "4":
-            $('#bg').attr('src', 'https://img.xjh.me/random_img.php'); //随机动漫
-            break;
-    }
+  var a = getBgImg();
+  switch (
+    ($("input[name='wallpaper-type'][value=" + a.type + "]").click(), a.type)
+  ) {
+    case "1":
+      var g = new Array();
+      (g[0] = "./img/background1.webp"),
+        (g[1] = "./img/background2.webp"),
+        (g[2] = "./img/background3.webp"),
+        (g[3] = "./img/background4.webp"),
+        (g[4] = "./img/background5.webp"),
+        (g[5] = "./img/background6.webp"),
+        (g[6] = "./img/background7.webp"),
+        (g[7] = "./img/background8.webp");
+      var i = Math.floor(8 * Math.random());
+      $("#bg").attr("src", g[i]);
+      break;
+    case "2":
+      $("#bg").attr("src", "https://api.dujin.org/bing/1920.php");
+      break;
+    case "3":
+      $("#bg").attr("src", "https://api.xsot.cn/bing?jump=true");
+      break;
+    case "4":
+      $("#bg").attr("src", "https://api.hanximeng.com/ranimg/api.php");
+  }
 }
-
+var bg_img_preinstall = { type: "1", path: "" };
 $(document).ready(function () {
-    // 壁纸数据加载
-    setBgImgInit();
-    // 设置背景图片
+  setBgImgInit(),
     $("#wallpaper").on("click", ".set-wallpaper", function () {
-        var type = $(this).val();
-        var bg_img = getBgImg();
-        bg_img["type"] = type;
-
-        if (type === "1") {
-            setBgImg(bg_img);
-            var pictures = new Array();
-            pictures[0] = './img/background1.webp';
-            pictures[1] = './img/background2.webp';
-            pictures[2] = './img/background3.webp';
-            pictures[3] = './img/background4.webp';
-            pictures[4] = './img/background5.webp';
-            pictures[5] = './img/background6.webp';
-            pictures[6] = './img/background7.webp';
-            pictures[7] = './img/background8.webp';
-            var rd = Math.floor(Math.random() * 8);
-            $('#bg').attr('src', pictures[rd]) //随机默认壁纸
-            iziToast.show({
-                message: '壁纸设置成功',
-            });
-        }
-
-        if (type === "2") {
-            setBgImg(bg_img);
-            $('#bg').attr('src', 'https://api.dujin.org/bing/1920.php'); //必应每日
-            iziToast.show({
-                message: '壁纸设置成功',
-            });
-        }
-
-        if (type === "3") {
-            setBgImg(bg_img);
-            $('#bg').attr('src', 'https://api.xsot.cn/bing?jump=true'); //随机风景
-            iziToast.show({
-                message: '壁纸设置成功',
-            });
-        }
-
-        if (type === "4") {
-            setBgImg(bg_img);
-            $('#bg').attr('src', 'https://api.ixiaowai.cn/api/api.php'); //随机动漫
-            iziToast.show({
-                message: '壁纸设置成功',
-            });
-        }
+      var a = $(this).val(),
+        g = getBgImg();
+      if (((g.type = a), "1" === a)) {
+        setBgImg(g);
+        var i = new Array();
+        (i[0] = "./img/background1.webp"),
+          (i[1] = "./img/background2.webp"),
+          (i[2] = "./img/background3.webp"),
+          (i[3] = "./img/background4.webp"),
+          (i[4] = "./img/background5.webp"),
+          (i[5] = "./img/background6.webp"),
+          (i[6] = "./img/background7.webp"),
+          (i[7] = "./img/background8.webp");
+        var e = Math.floor(8 * Math.random());
+        $("#bg").attr("src", i[e]), iziToast.show({ message: "壁纸设置成功" });
+      }
+      "2" === a &&
+        (setBgImg(g),
+        $("#bg").attr("src", "https://bing.img.run/1920x1080.php"),
+        iziToast.show({ message: "壁纸设置成功" })),
+        "3" === a &&
+          (setBgImg(g),
+          $("#bg").attr("src", "https://api.xsot.cn/bing?jump=true"),
+          iziToast.show({ message: "壁纸设置成功" })),
+        "4" === a &&
+          (setBgImg(g),
+          $("#bg").attr("src", "https://api.hanximeng.com/ranimg/api.php"),
+          iziToast.show({ message: "壁纸设置成功" }));
     });
 });
